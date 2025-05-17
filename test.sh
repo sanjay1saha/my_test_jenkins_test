b9e1de38143aaae7c3a0a7e9e4b53af5ce404d41
sanjay1saha <42437704+sanjay1saha@users.noreply.github.com>
2025-05-17 10:27:25 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Running tests..."
+# Simulate a test
+if python3 my_app/app.py | grep -q "Hello, World!"; then
+  echo "Test passed."
+  exit 0
+else
+  echo "Test failed."
+  exit 1
+fi
